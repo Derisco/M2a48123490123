0444189172eec5c58aaea259a51dc087db42e0ad
Erik Hattervig <erik.hattervig@gmail.com>
2017-07-27 18:58:31 -0500

    Added IdentityRole.cs to SQL project

---
+++ LanguagePace.MySQL/IdentityRole.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.AspNet.Identity;
+
+namespace LanguagePace.MySQL
+{
+    /// <summary>
+    ///     Implements the ASP.NET Identity IRole Interface
+    /// </summary>
+    class IdentityRole : IRole
+    {
+        /// <summary>
+        ///     Default constructor for Role
+        /// </summary>
+        public IdentityRole()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+
+        /// <summary>
+        ///     Constructor that takes name as argument
+        /// </summary>
+        /// <param name="name"></param>
+        public IdentityRole(string name) : this()
+        {
+            Name = name;
+        }
+
+        /// <summary>
+        ///     Constuctor that taks name and id as arguments
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="id"></param>
+        public IdentityRole(string name, string id)
+        {
+            Name = name;
+            Id = id;
+        }
+
+        /// <summary>
+        ///     Role Id
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        ///     Role name
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
