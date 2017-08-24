793e9cb6956f6cce55eebd60e2756ee037c94911
Erik Hattervig <erik.hattervig@gmail.com>
2017-08-24 13:01:30 -0500

    Added unit tests to AccountControllerTest

--- LanguagePace.Tests/Controllers/AccountControllerTest.cs
+++ LanguagePace.Tests/Controllers/AccountControllerTest.cs
@@ -8,6 +8,11 @@ August 23rd 2017
 
 using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using LanguagePace.Controllers;
+using LanguagePace.Models;
+using LanguagePace.Identity;
+using LanguagePace.MySQL;
 
 namespace LanguagePace.Tests.Controllers
 {
@@ -15,8 +20,67 @@ namespace LanguagePace.Tests.Controllers
     public class AccountControllerTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void RegisterView()
         {
+            // Arrange
+            AccountController controller = new AccountController();
+
+            // Act
+            ViewResult result = controller.Register() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
         }
+
+        [TestMethod]
+        public void RegisterAction()
+        {
+            // Arrange
+            AccountController controller = new AccountController();
+
+            RegisterViewModel model = new RegisterViewModel()
+            {
+                Email = "test@languagepace.com",
+                Username = "testUser",
+                Password = "PasswordTest1!",
+                ConfirmPassword = "PasswordTest1!"
+            };
+
+            // Act
+
+            MySQLDatabase db = new MySQLDatabase("LanguagePaceDB");
+            db.Execute("delete from languagepace.user where email='test@languagepace.com'", null);
+
+            try
+            {
+                ViewResult result = controller.Register(model).Result as ViewResult;
+
+                UserStore<IdentityUser> userStore = new UserStore<IdentityUser>(new MySQLDatabase("LanguagePaceDB"));
+                IdentityUser user = userStore.FindByEmailAsync("test@languagepace.com").Result;
+
+                // Assert
+                Assert.IsNotNull(result);
+                Assert.IsNotNull(user);
+            }
+            finally
+            {
+                db.Execute("delete from languagepace.user where email='test@languagepace.com'", null);
+
+            }
+        }
+
+        [TestMethod]
+        public void ForgotPasswordView()
+        {
+            // Arrange
+            AccountController controller = new AccountController();
+
+            // Act
+            ViewResult result = controller.ForgotPassword() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
     }
 }
