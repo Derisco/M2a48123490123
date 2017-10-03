c7a63bdb0fd2a8a015cb359bd2bfb02cb0e62c3d
Erik Hattervig <erik.hattervig@gmail.com>
2017-10-03 15:43:17 -0500

    Added AutoMapper to project

---
+++ LanguagePace.Web/App_Start/AutoMapperConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+
+namespace LanguagePace
+{
+    /// <summary>
+    ///     Auto mapping configuations go here
+    /// </summary>
+    public static class AutoMapperConfig
+    {
+        /// <summary>
+        ///     AutoMapper mappings should be created here
+        /// </summary>
+        public static void RegisterMappings()
+        {
+            Mapper.Initialize(cfg => { 
+                cfg.CreateMap<Identity.IdentityUser, Models.ApplicationUser>()
+                    .ReverseMap();
+            });
+        }
+
+    }
+}
---
+++ LanguagePace.Web/Helpers/Validation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LanguagePace.Helpers
+{
+    static public class Validation
+    {
+        /// <summary>
+        ///     Checks if the string is a valid email
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns></returns>
+        static public bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+
+
+    }
+}
